cb05e162b7721f2bfb5b1b4ffbc4f1b73478298c
Swadu11 <swadeshwarko@gmail.com>
2025-06-13 20:25:33 +0530

    first Commit

---
+++ src/App.jsx
@@ -0,0 +1,64 @@
+import React, { useState, useEffect} from "react";
+import Header from "../src/components/Header";
+import Footer from "../src/components/Footer";
+import Note from "../src/components/Note";
+import CreateArea from "../src/components/CreateArea";
+impoer
+
+function App() {
+
+  const[notes, setNotes]=useState(()=>{
+    const savedNotes = localStorage.getItem("notes");
+    return savedNotes ? JSON.parse(savedNotes) : [];
+  })
+
+  useEffect(() => {
+    localStorage.setItem("notes", JSON.stringify(notes));
+  }, [notes]);
+
+  const handleAdd=(newNote)=>{
+    setNotes([...notes,newNote])
+  }
+
+  //   const addNote = (newNote) => {
+  //   setNotes((prevNotes) => [...prevNotes, newNote]);
+  // };
+
+
+  // const handleDelete=(index)=>{
+  //   const update=[...notes]
+  //   update.splice(index,1)
+  //   setNotes(update)
+  // }
+
+const handleDelete=(id)=>{
+setNotes((prevNotes) => prevNotes.filter((_, index) => index !== id));
+}
+  
+
+
+  return (
+    <div>
+      <Header />
+      <CreateArea onAdd={handleAdd}/>
+      {/* <Note key={1} title="Note title" content="Note content" onDelelte={handleDelete}/> */}
+      {notes.map((note,index)=>(
+        <Note key={index} id={index} title={note.title} content={note.content} onDelete={handleDelete}
+        />
+      ))}
+       {/* {notes.map((note, index) => (
+        <Note
+          key={index}
+          id={index}
+          title={note.title}
+          content={note.content}
+          onDelete={handleDelete}
+        />
+      ))} */}
+      <Footer />
+
+    </div>
+  );
+}
+
+export default App;
